76f1bdeed5044fc0cdc5cee93a2445c6a434856f
Yunsheng Bai <yba@ucla.edu>
2017-07-07 18:49:01 -0700

    Modify p info parser

--- code/pInfoParser.py
+++ code/pInfoParser.py
@@ -1,7 +1,8 @@
 '''
 Patient Info Parser.
 '''
-import glob
+from __future__ import division
+import glob, re
 from nltk import sent_tokenize, word_tokenize
 
 MALE = set(['male', 'man', 'dad', 'daddy', 'father', 'grandfather', \
@@ -12,7 +13,7 @@ FEMALE = set(['female', 'woman', 'mom', 'mother', 'mommy', 'grandmother', \
 
 def genderIdentification(sent):
   rtn = []
-  for word in word_tokenize(sent):
+  for word in word_tokenize(sent): # split a sentence into words
     checkWord(word, MALE, 'male', rtn)
     checkWord(word, FEMALE, 'female', rtn)
   return rtn
@@ -23,22 +24,82 @@ def checkWord(word, set, label, li):
       li.append({'gender': label, 'gender_found_from': w})
 
 def testAllDocsIn(dir):
-  files = glob.glob('%s/*.txt' % dir)
+  normal_docs = []
+  conflicted_g_info_docs = []
+  no_g_info_docs = []
+  docs = {'normal': normal_docs, 'no_g_info': conflicted_g_info_docs, \
+  'conflicted_g_info': no_g_info_docs}
+  files = sort_nicely(glob.glob('%s/*.txt' % dir))
   for filename in files:
     with open(filename, 'r') as fin:
-      p_ = False
-      p = filename+'-'*50+'\n'
+      doc = {'id': filename.split('/')[-1]}
+      g_info = []
+      doc['g_info'] = g_info
+      genders = set()
       for line in fin:
-        for sent in sent_tokenize(line):
-          r = genderIdentification(sent)
-          if r:
-            p += '%s\t%s\n' % (r, sent)
-            if len(r) > 1:
-              p_ = True
-    if p_:
-      print p
-  print 'Processed %s files' % len(files)
+        for sent in sent_tokenize(line): # split a document into sentences
+          g = genderIdentification(sent)
+          if g:
+            g_info.append({'sent': sent, 'sent_g_info': g})
+            for x in g:
+              genders.add(x['gender'])
+    # Classify the document.
+    if len(genders) == 1:
+      normal_docs.append(doc)
+    elif len(genders) == 2:
+      conflicted_g_info_docs.append(doc)
+    elif len(genders) == 0:
+      no_g_info_docs.append(doc)
+    else:
+      raise RuntimeError('Should not see this')
+  lm = len(normal_docs)
+  ln = len(no_g_info_docs)
+  lc = len(conflicted_g_info_docs)
+  lt = len(files)
+  print '%s files are normal:' % lm
+  print [d['id'] for d in normal_docs]
+  print
+  print '%s files have no gender detected:' % ln
+  print [d['id'] for d in no_g_info_docs]
+  print
+  print '%s files have conflicted gender info detected:' % lc
+  print [d['id'] for d in conflicted_g_info_docs]
+  print
+  for d in conflicted_g_info_docs:
+    print d['id']+'-'*50
+    for sent in d['g_info']:
+      print '%s\t%s\n' % (sent['sent_g_info'], sent['sent'])
+  print '%s + %s + %s = %s'%(lm, ln, lc, lt)
+  print '%s / %s = %s' % (lm, lt, lm / lt)
+  print '%s / %s = %s' % (ln, lt, ln / lt)
+  print '%s / %s = %s' % (lc, lt, lc / lt)
+  print 'Processed %s files in %s' % (lt, dir)
 
+'''
+Code below is from 
+https://stackoverflow.com/questions/4623446/how-do-you-sort-files-numerically.
+'''
+def tryint(s):
+    try:
+        return int(s)
+    except:
+        return s
+
+def alphanum_key(s):
+    """ Turn a string into a list of string and number chunks.
+        "z23a" -> ["z", 23, "a"]
+    """
+    return [ tryint(c) for c in re.split('([0-9]+)', s) ]
+
+def sort_nicely(l):
+    """ Sort the given list in the way that humans expect.
+    """
+    l.sort(key=alphanum_key)
+    return l
+
+'''
+End of borrowing.
+'''
 
 if __name__ == '__main__':
   testAllDocsIn('/home/yba/Documents/summer_2017/Arrhythmias/Arrhythmias')
